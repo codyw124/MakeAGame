adbf7ae3142a1f74b70ab7518494decbdb6f877d
codyw124 <codyw124@gmail.com>
2019-02-02 18:14:45 -0500

    commit

--- Geometry2D.h
+++
@@ -1,5 +0,0 @@
-//#include "Vector2.h"
-
-class Geometry2D //: public Vector2
-{
-}
---
+++ Renderable.h
@@ -0,0 +1,16 @@
+class Renderable
+{
+private:
+
+public:
+    Renderable();
+    ~Renderable();
+};
+
+Renderable::Renderable()
+{
+}
+
+Renderable::~Renderable()
+{
+}
