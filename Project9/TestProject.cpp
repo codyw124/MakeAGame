cd5de001c58bcecf53f53fbb9e373c848a466b11
codyw124 <codyw124@gmail.com>
2019-04-29 22:00:53 -0400

    timer stuff

--- Project9/TestProject.cpp
+++ Project9/TestProject.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include "Window.h"
 #include "Character.h"
+#include "Timer.h"
 
 using namespace std;
 
 void runTests();
 void runTestOne();
 void runTestTwo();
+void runTestThree();
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // runTests();
-    runTestTwo();
+    // runTestTwo();
+    runTestThree();
     return 0;
 }
 
@@ -19,57 +22,58 @@ void runTests()
 {
     runTestOne();
     runTestTwo();
+    runTestThree();
 }
 
 void runTestOne()
 {
-    cout<<"test 1" <<endl;
+    cout << "test 1" << endl;
 
     //make a window  with this width and height
-    Window x = Window(640,480, "test window");
+    Window x = Window(640, 480, "test window");
 
-    if(x.getHeight() != 480)
+    if (x.getHeight() != 480)
     {
-        cout<<"get height is busted" <<endl;
+        cout << "get height is busted" << endl;
     }
 
-    if(x.getWidth() != 640)
+    if (x.getWidth() != 640)
     {
-        cout<<"get width is busted" <<endl;
+        cout << "get width is busted" << endl;
     }
 
-    if(x.getName() != "test window")
+    if (x.getName() != "test window")
     {
-        cout<<"get name is busted" <<endl;
+        cout << "get name is busted" << endl;
     }
 
-    if(x.isOpen() != true)
+    if (x.isOpen() != true)
     {
-        cout<< "is open is preclose busted" <<endl;
+        cout << "is open is preclose busted" << endl;
     }
 
     //window should wait for you to close it
-    cout<<"window should have waited for you to close it"<<endl;
+    cout << "window should have waited for you to close it" << endl;
     x.runMainLoop();
 
     x.free();
 
-    if(x.isOpen() != false)
+    if (x.isOpen() != false)
     {
-        cout<< "is open after close is busted" <<endl;
+        cout << "is open after close is busted" << endl;
     }
 }
 
 void runTestTwo()
 {
-    cout<<"test 2" <<endl;
+    cout << "test 2" << endl;
 
     //make a window  with this width and height
-    Window window = Window(240,160, "test window");
+    Window window = Window(240, 160, "test window");
 
     Character character = Character();
 
-    character.loadSpriteSheet("./Goku.png",23,23,32,32,507, window.getRenderer());
+    character.loadSpriteSheet("./Goku.png", 23, 23, 32, 32, 507, window.getRenderer());
 
     std::queue<int> standingDownAnimation = std::queue<int>();
     standingDownAnimation.push(0);
@@ -111,4 +115,56 @@ void runTestTwo()
     window.setPlayer(character);
 
     window.runMainLoop();
+}
+
+void runTestThree()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //The frames per second timer
+    Timer fpsTimer;
+
+    //In memory text stream
+    std::stringstream timeText;
+
+    //Start counting frames per second
+    int countedFrames = 0;
+    fpsTimer.start();
+
+    //While application is running
+    while (!quit)
+    {
+        //Handle events on queue
+        while (SDL_PollEvent(&e) != 0)
+        {
+            //User requests quit
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        //Calculate and correct fps
+        float avgFPS = countedFrames / (fpsTimer.getTicks() / 1000.f);
+        if (avgFPS > 2000000)
+        {
+            avgFPS = 0;
+        }
+
+        //Set text to be rendered
+        timeText.str("");
+        timeText << "Average Frames Per Second " << avgFPS;
+
+        std::cout << avgFPS << std::endl;
+
+        ++countedFrames;
+    }
+
+    SDL_Quit();
 }
