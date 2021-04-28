f7127307d8e130c353f047cbbb7dc174ea38ed4b
Cody Ware <39965219+codyw124@users.noreply.github.com>
2021-04-28 16:07:16 -0400

    Delete MakeAGame/ARPG/.vshistory directory

--- MakeAGame/ARPG/.vshistory/EventHandler.h/2020-02-03_15_35_31_237.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SDL.h>
-
-class EventHandler
-{
-public:
-	virtual void handleEvent(SDL_Event* event, SDL_Renderer* renderer) = 0;
-};
--- MakeAGame/ARPG/.vshistory/ItemDropper.h/2020-02-03_14_25_13_777.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SDL.h>
-
-class ItemDropper
-{
-public:
-	virtual void dropItems(SDL_Renderer* renderer) = 0;
-};
